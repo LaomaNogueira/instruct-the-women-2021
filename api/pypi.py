a7bd0d3da228e831fbe6bae123c4fd467c6789b1
Laoma Nogueira <laomanogueira@gmail.com>
2021-10-26 21:25:24 +0000

    Últimos ajustes e limpeza do código

--- api/pypi.py
+++ api/pypi.py
@@ -1,18 +1,8 @@
 import requests
 
-# Referências sobre o uso do requests:
-#
-# Fazendo requisições:
-# https://docs.python-requests.org/en/master/user/quickstart/#make-a-request
-# Usando JSON retornado:
-# https://docs.python-requests.org/en/master/user/quickstart/#json-response-content
-
 
 def version_exists(package_name, version):
-    # TODO
-    # Fazer requisição na API do PyPI para checar se a versão existe
-    print('segundo_com versão')
-    url = ('https://pypi.org/pypi/{0}/{1}/json'.format(package_name, version))
+    url = ("https://pypi.org/pypi/{0}/{1}/json".format(package_name, version))
     r = requests.get(url)
     
     if r.status_code == 404:
@@ -22,19 +12,13 @@ def version_exists(package_name, version):
 
 
 def latest_version(package_name):
-    # TODO
-    # Fazer requisição na API do PyPI para descobrir a última versão
-    # de um pacote. Retornar None se o pacote não existir.
-    print('segundo_sem versão')
-    url = ('https://pypi.org/pypi/{0}/json'.format(package_name))
+    url = ("https://pypi.org/pypi/{0}/json".format(package_name))
     r = requests.get(url)
 
     if r.status_code == 404:
         return None
 
     package = r.json()
-    
-    package_version = package['info']['version']
-
-    return {'name': package_name, 'version': package_version}
+    package_version = package["info"]["version"]
+    return {"name": package_name, "version": package_version}
 
--- api/serializers.py
+++ api/serializers.py
@@ -11,25 +11,19 @@ class PackageSerializer(serializers.ModelSerializer):
         extra_kwargs = {"version": {"required": False}}
 
     def validate(self, data):
-        # TODO
-        # Validar o pacote, checar se ele existe na versão especificada.
-        # Buscar a última versão caso ela não seja especificada pelo usuário.
-        # Subir a exceção `serializers.ValidationError()` se o pacote não
-        # for válido.
-        print('primeiro com data %s' %data)
         
-        if 'version' not in data:
-            validated_data = latest_version(data['name'])
+        if "version" not in data:
+            validated_data = latest_version(data["name"])
 
             if validated_data == None:
-                serializers.ValidationError()
+                raise serializers.ValidationError({"error": "One or more packages doesn't exist"})
 
             return validated_data
         
-        if not version_exists(data['name'], data['version']):
-            serializers.ValidationError()
+        if not version_exists(data["name"], data["version"]):
+            raise serializers.ValidationError({"error": "One or more packages doesn't exist"})
 
-        validated_data = {'name': data['name'], 'version': data['version']}
+        validated_data = {"name": data["name"], "version": data["version"]}
         return validated_data
 
 
@@ -41,23 +35,19 @@ class ProjectSerializer(serializers.ModelSerializer):
     packages = PackageSerializer(many=True)
 
     def create(self, validated_data):
-        # TODO
-        # Salvar o projeto e seus pacotes associados.
-        #
-        # Algumas referência para uso de models do Django:
-        # - https://docs.djangoproject.com/en/3.2/topics/db/models/
-        # - https://www.django-rest-framework.org/api-guide/serializers/#saving-instances
 
-        print('terceiro com data %s' %validated_data)
-        print(self)
-        project = Project(name=validated_data['name'])
-        print(project.id)
-        project.save()
+        project = Project.objects.filter(name=validated_data["name"]).first()
         
-        print(project)
+        if project == None:
+            project = Project(name=validated_data["name"])
+            project.save()
 
-        for package in validated_data['packages']:
-            package_release = PackageRelease(project_id=project.id, name=package['name'], version=package['version'])
+        for package in validated_data["packages"]:
+            package_release = PackageRelease(
+                project_id=project.id, 
+                name=package["name"], 
+                version=package["version"]
+            )
             package_release.save()
         
         return project
